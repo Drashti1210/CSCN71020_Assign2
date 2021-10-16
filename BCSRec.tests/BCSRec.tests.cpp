d3203faf63519b92c8a2d1974297d6887d7e315c
Drashti Bhaveshkumar Patel <drashti12102000@gmail.com>
2021-10-16 12:35:55 -0400

    Written Test for getArea function.

--- BCSRec.tests/BCSRec.tests.cpp
+++ BCSRec.tests/BCSRec.tests.cpp
@@ -2,7 +2,9 @@
 #include "CppUnitTest.h"
 
 extern"C" int getPerimeter(int* length, int* width);
-
+extern"C" int getArea(int* length, int* width);
+extern"C" void setLength(int input, int* length);
+extern"C" void setWidth(int input, int* width);
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace BCSRectests
@@ -12,6 +14,7 @@ namespace BCSRectests
 	public:
 		TEST_METHOD(PerimeterTest1)
 		{
+			// testing the 
 			int length = 1;
 			int width = 1;
 			const int EXPECTED = 4;
@@ -21,6 +24,19 @@ namespace BCSRectests
 			
 			Assert::AreEqual(EXPECTED, actual);
 		}
+
+		TEST_METHOD(AreaTest1)
+		{
+			int length = 5;
+			int width = 4;
+			const int EXPECTED = 20;
+			int actual;
+
+			actual = getArea(&length, &width);
+
+			Assert::AreEqual(EXPECTED, actual);
+		}
+
 
 	
 
