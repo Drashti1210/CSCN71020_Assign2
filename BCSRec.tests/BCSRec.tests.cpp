971cf1cf611b08ac91cc009553402b990e1ab4e5
Drashti Bhaveshkumar Patel <drashti12102000@gmail.com>
2021-10-16 12:46:14 -0400

    Written test for setLength function.

--- BCSRec.tests/BCSRec.tests.cpp
+++ BCSRec.tests/BCSRec.tests.cpp
@@ -36,6 +36,33 @@ namespace BCSRectests
 
 			Assert::AreEqual(EXPECTED, actual);
 		}
+		TEST_METHOD(LengthTest1)
+		{
+			int input = 1;
+			int length = 4;
+
+			setLength(input, &length);
+
+			Assert::AreEqual(input, length);
+		}
+		TEST_METHOD(LengthTest2)
+		{
+			int input = 99;
+			int length = 4;
+
+			setLength(input, &length);
+
+			Assert::AreEqual(input, length);
+		}
+		TEST_METHOD(LengthTest3)
+		{
+			int input = -2;
+			int length = 5;
+
+			setLength(input, &length);
+
+			Assert::AreEqual(input, length);
+		}
 
 
 	
